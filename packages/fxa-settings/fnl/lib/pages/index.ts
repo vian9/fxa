210f9dc173057e78f148961eddcd085ecd194c16
Danny Coates <dannycoates@gmail.com>
2021-04-29 19:31:29 -0700

    new bugfix and test

--- packages/fxa-settings/fnl/lib/pages/index.ts
+++ packages/fxa-settings/fnl/lib/pages/index.ts
@@ -5,6 +5,7 @@ import { DisplayNamePage } from './displayName';
 import { LoginPage } from './login';
 import { RecoveryKeyPage } from './recoveryKey';
 import { RelierPage } from './relier';
+import { SecondaryEmailPage } from './secondaryEmail';
 import { SettingsPage } from './settings';
 import { TotpPage } from './totp';
 
@@ -14,6 +15,7 @@ export function create(page: Page, env: BaseEnv) {
     changePassword: new ChangePasswordPage(page, env),
     displayName: new DisplayNamePage(page, env),
     login: new LoginPage(page, env),
+    secondaryEmail: new SecondaryEmailPage(page, env),
     settings: new SettingsPage(page, env),
     recoveryKey: new RecoveryKeyPage(page, env),
     relier: new RelierPage(page, env),
